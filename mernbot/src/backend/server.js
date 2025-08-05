637442590b7e6798246de8fda17bdb7345993e93
aliyan16 <aliyanm12376@gmail.com>
2025-08-05 13:39:09 +0500

    setup

---
+++ mernbot/src/backend/server.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+require('dotenv').config()
+
+const openAiRoutes=require('../models/routes/openai')
+
+const app=express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('api/openai',openAiRoutes)
+
+
+
+const PORT=process.env.PORT || 5000
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
